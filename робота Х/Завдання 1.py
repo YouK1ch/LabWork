10e4fcb7691ac4ad471480eaffc0320f2ff84ac2
YouK1ch <deni.s201811111111@gmail.com>
2024-11-18 01:46:13 +0200

    Update Завдання 1.py

--- робота Х/Завдання 1.py
+++ робота Х/Завдання 1.py
@@ -1,16 +1,16 @@
 import psycopg2
 from psycopg2 import sql
 
-# Параметри підключення до бази даних
+
 db_config = {
     'dbname': 'test',
     'user': 'postgres',
     'password': '2281488',
-    'host': 'localhost',  # або інший хост
-    'port': '5432'        # стандартний порт PostgreSQL
+    'host': 'localhost',  
+    'port': '5432'        
 }
 
-# Підключення до бази даних
+
 try:
     connection = psycopg2.connect(**db_config)
     cursor = connection.cursor()
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS users (
 );
 '''              
 
-    # Виконання запиту
+  
     cursor.execute(create_table_query)
     connection.commit()
     print("Таблиця 'users' була успішно створена.")
@@ -33,7 +33,7 @@ except Exception as e:
     print(f"Виникла помилка: {e}")
 
 finally:
-    # Закриття курсора та з'єднання
+    
     if cursor:
         cursor.close()
     if connection:
